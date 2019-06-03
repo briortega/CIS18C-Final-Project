63dc044de9ff824b683b8807b5184b05eb946451
briortega <31421284+briortega@users.noreply.github.com>
2019-06-03 00:30:11 -0700

    Adding Alejandra's user sort & search to students in progress

--- Student/src/student/Student.java
+++ Student/src/student/Student.java
@@ -8,10 +8,12 @@ public class Student {
     public static void main(String[] args) {
         HashMap<Integer, String> studentList = new HashMap<Integer,String>();
         Scanner input = new Scanner(System.in);
-        
+        Students users = new Students();
+
         System.out.println("1) Login");
-        System.out.println("2) Register");
-        System.out.println("3) Exit");
+        System.out.println("2) Register & show list");
+        System.out.println("3) Search for Student");
+        System.out.println("4) Exit");
         System.out.print("Enter your choice: ");
         Integer choice = Integer.parseInt(input.nextLine());
         
@@ -37,14 +39,28 @@ public class Student {
                 System.out.print("Enter your id number: ");
                 Integer idNumber = Integer.parseInt(input.nextLine());
                 studentList.putIfAbsent(idNumber,name);
-        }
+                //System.out.println(users.addUser(studentList.get(name), studentList.get(idNumber)));
+                System.out.println(users.addUser(name, idNumber));
+                System.out.println("Displaying Users.");
+        	System.out.println(users.displayUsers());
+                System.out.println("Displaying users after sorting by idNumber.");
+                users.sortUsers();
+                System.out.println(users.displayUsers());
+            }else if (choice == 3){
+               // Students users = new Students();
+                //System.out.println(users.addUser(studentList.get(name), studentList.get(idNumber)));
+                System.out.print("Searching for user with id: ");
+                Integer idSearch = Integer.parseInt(input.nextLine());
+                System.out.println("User is : " + users.searchById(idSearch));
+            }
         System.out.println(studentList);
         System.out.println("1) Login");
-        System.out.println("2) Register");
-        System.out.println("3) Exit");
+        System.out.println("2) Register & show list");
+        System.out.println("3) Search for Student");
+        System.out.println("4) Exit");
         System.out.print("Enter your choice: ");
         choice = Integer.parseInt(input.nextLine());
-    }while(choice != 3);
+    }while(choice != 4);
         System.out.println("Student list: " + studentList);
     }
 }
--- Student/src/student/Students.java
+++ Student/src/student/Students.java
@@ -1,13 +1,17 @@
 package student;
 
+import java.util.ArrayList;
+
 public class Students {
     protected String name;
-    protected String email;
     protected Integer idNumber;
 
-    public void Students(String name, String email, Integer idNumber){
+    //public void Students(String name, String email, Integer idNumber){
+    //    this.name = name;
+  //      this.idNumber = idNumber;
+    //}
+    private Students(String name, int idNumber) {
         this.name = name;
-        this.email = email;
         this.idNumber = idNumber;
     }
     public String getName() {
@@ -26,12 +30,129 @@ public class Students {
         this.idNumber = idNumber;
     }
     
-    public String getEmail() {
-        return email;
+    private ArrayList<Students> users;
+
+
+    //Constructor for Users public class
+    public Students() 
+    {
+        users = new ArrayList<Students>();
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+
+    public String addUser(String name, int id) 
+    {
+	try
+        {
+            Students tmp = new Students(name, id);
+            users.add(tmp);
+            return "User added!";
+	} 
+        catch(Exception e) 
+        {
+            return "Error adding user: " + e;
+	}
+    }
+
+
+    // Show users
+    public String displayUsers() 
+    {
+	String ans = "";
+	for(Students u : users) 
+        {
+            ans += u.getName() + ", id: " + u.getIdNumber() + ".\n";
+	}
+	return ans;
+    }
+
+
+    // calls binary search, and decides what to return depending on return value
+    public String searchById(int id)
+    {
+	String ans = binarySearch(0, users.size()-1, id);
+	if(ans == null)
+        {
+            return "User not found!";
+        }
+	else
+        {
+            return ans;
+        }
+    }
+
+
+    // binary search performed on the already sorted users arrayList.
+    private String binarySearch(int start, int end, int target)
+    {
+	if(start > end)
+        {
+            return null;
+	}
+	int mid = start + (end-start)/2;
+	//System.out.println("start: " + start + ", mid: " + mid +", end: " + end);
+	if(users.get(mid).getIdNumber() == target) 
+        {
+            return users.get(mid).getName();
+        }
+	if(target > users.get(mid).getIdNumber())
+        {
+            return binarySearch(mid+1, end, target);
+        }
+	else return binarySearch(start, mid-1, target);
+    }
+
+
+
+
+    // Sort users by id, call quickSort
+    public void sortUsers()
+    {
+	quickSort(0, users.size()-1);
+    }
+
+
+    // quicksort
+    private void quickSort(int lo, int hi)
+    {
+	// if the the part of the array has already been sorted
+	if(lo >= hi)
+        {
+            return;
+        }
+	int p = partition(lo, hi);
+	quickSort(lo, p-1);
+	quickSort(p+1, hi);
+    }
+
+
+    /* 
+    * To find out where the index of partition will be.
+    * For simplicity purposes we will always choose pivot to be the last element,
+    * but the effiency could be improved by choosing a random pivot.
+    */
+    private int partition(int lo, int hi)
+    {
+	int i = lo;
+	for(int j = i; j < hi; j++)
+        {
+            if(users.get(j).getIdNumber() < users.get(hi).getIdNumber())
+            {
+		swap(i, j);
+		i++;
+            }
+	}
+	swap(i, hi);
+	return i;
+    }
+
+
+    // to swap elements in the users array list
+    private void swap(int i, int j)
+    {
+        Students tmp = users.get(i);
+	users.set(i, users.get(j));
+	users.set(j, tmp);
     }
 
 }
